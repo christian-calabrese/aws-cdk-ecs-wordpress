f55144573ba2c7f2d2b58a594c24d8fd2ee57885
Christian Calabrese <christian.calabrese@outlook.com>
2021-11-16 18:34:42 +0100

    feature(database_stack): added custom kms encryption feature

--- infrastructure/stacks/database_stack.py
+++ infrastructure/stacks/database_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_ec2 as ec2,
+    aws_kms as kms,
     aws_rds as rds,
     core
 )
@@ -12,6 +13,11 @@ class DatabaseStack(core.NestedStack):
     def __init__(self, scope: core.Construct, id: str, params, vpc_stack: VpcStack,
                  **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        if params.aurora.custom_kms_encrypted:
+            self.kms_key = kms.Key(self, "Wordpress-KMS-RDS-Key")
+        else:
+            self.kms_key = None
+
         if params.aurora.serverless:
             self.database = rds.ServerlessCluster(
                 self, "Wordpress-RDS-Aurora-Serverless",
@@ -22,9 +28,11 @@ class DatabaseStack(core.NestedStack):
                     auto_pause=core.Duration.seconds(params.aurora.get("auto_pause_sec", 0))
                 ),
                 deletion_protection=False,
-                backup_retention=core.Duration.days(7),
+                backup_retention=core.Duration.days(params.aurora.get("backup_retention_days", 1)),
                 removal_policy=core.RemovalPolicy.SNAPSHOT,
-                subnet_group=ec2.SubnetSelection(subnet_type=ec2.SubnetType('PRIVATE')).subnets
+                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType('PRIVATE')).subnets,
+                storage_encryption_key=self.kms_key
             )
         else:
+            # TODO: Implement serverful database cluster with or without read replica
             self.database = None
